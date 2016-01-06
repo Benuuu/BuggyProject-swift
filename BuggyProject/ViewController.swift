b32bdb04b0ad7600dd2cf0ad04831a487bcab939
Ben Wu <ben@intrepid.io>
2016-01-06 16:05:22 -0500

    Initial Commit

---
+++ BuggyProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BuggyProject
+//
+//  Created by Benjamin Wu on 1/6/16.
+//  Copyright © 2016 IntrepidPursuits. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
